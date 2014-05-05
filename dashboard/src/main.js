72c8160a0d5cbd11e36db37be39bf5c46bbfc291
Alexandre Vallette <vallettea@gmail.com>
2014-05-06 01:50:37 +0200

    circles and zoom level. fixes #4

--- dashboard/src/main.js
+++ dashboard/src/main.js
@@ -2,7 +2,7 @@
     'use strict';
     var BORDEAUX_COORDS = [44.84, -0.57];
 
-    var map = L.map('map').setView(BORDEAUX_COORDS, 11);
+    var map = L.map('map').setView(BORDEAUX_COORDS, 12);
 
     L.tileLayer('http://api.tiles.mapbox.com/v3/vallettea.hkjjf19g/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'

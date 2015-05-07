ade17858568ef5ccb78dd6bb283528a361ef95e4
David Bruant <bruant.d@gmail.com>
2015-05-07 18:36:29 +0200

    using sql module for query string generation

--- database/models/sensors.js
+++ database/models/sensors.js
@@ -1,27 +1,31 @@
 "use strict";
 
 var sql = require('sql');
+sql.setDialect('postgres');
 var databaseP = require('../management/databaseClientP');
 
+var sensors = sql.define({
+    name: 'sensors',
+    columns: ['id', 'name']
+});
 
 module.exports = {
-    create: function(data){
-        return databaseP.then(function(db){
+    create: function (data) {
+        return databaseP.then(function (db) {
             
-            var query = [
-                "INSERT INTO",
-                "sensors",
-                "("+"name"+")",
-                "VALUES",
-                "('"+data.name+"')",
-                "RETURNING id"
-            ].join(' ') + ';';
+            var query = sensors
+                .insert({
+                    name: 'bla'
+                })
+                .returning('id')
+                .toQuery();
 
             console.log('sensors create query', query);
-            
-            return new Promise(function(resolve, reject){
-                db.query(query, function(err, result){
-                    if(err) reject(err); else resolve(result.rows[0].id);
+
+            return new Promise(function (resolve, reject) {
+                db.query(query, function (err, result) {
+                    if (err) reject(err);
+                    else resolve(result.rows[0].id);
                 });
             });
         })

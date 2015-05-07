4c2f67e3ff8dfdd133a585ed6c147c4e8c3e8396
David Bruant <bruant.d@gmail.com>
2015-05-07 18:24:22 +0200

    communication with db works. Sensor created in DB

---
+++ database/management/createTables.sql
@@ -0,0 +1,40 @@
+-- useful things --
+
+CREATE TABLE IF NOT EXISTS lifecycle(
+    created_at  timestamp without time zone default current_timestamp,
+    updated_at  timestamp without time zone default current_timestamp
+);
+
+-- http://www.revsys.com/blog/2006/aug/04/automatically-updating-a-timestamp-column-in-postgresql/
+CREATE OR REPLACE FUNCTION update_updated_at_column()	
+RETURNS TRIGGER AS $$
+BEGIN
+    NEW.updated_at = now();
+    RETURN NEW;	
+END;
+$$ language 'plpgsql';
+
+
+
+-- Business tables --
+
+CREATE TABLE IF NOT EXISTS sensors (
+    id          SERIAL PRIMARY KEY,
+    name        VARCHAR(100) NOT NULL
+    -- type         some_enum -- affluence/bin-level
+) INHERITS(lifecycle);
+CREATE TRIGGER updated_at_sensors BEFORE UPDATE ON sensors FOR EACH ROW EXECUTE PROCEDURE update_updated_at_column();
+
+
+CREATE TABLE IF NOT EXISTS sensor_measurements (
+    id           SERIAL PRIMARY KEY,
+    sensor_id    integer REFERENCES sensors (id) NOT NULL,
+    amount       integer NOT NULL,
+    measurement_date    timestamp without time zone NOT NULL
+) INHERITS(lifecycle);
+CREATE TRIGGER updated_at_sensor_measurements BEFORE UPDATE ON sensor_measurements FOR EACH ROW EXECUTE PROCEDURE update_updated_at_column();
+
+
+-- CREATE INDEX name ON table (column);
+-- CREATE TYPE name AS ENUM ('v1', 'v2');
+
---
+++ database/management/dropAllTables.sql
@@ -0,0 +1,6 @@
+-- Destruction --
+
+DROP TABLE IF EXISTS sensor_measurements;
+DROP TABLE IF EXISTS sensors;
+DROP TABLE IF EXISTS lifecycle;
+

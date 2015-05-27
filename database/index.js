7ae9d570544cfd9d1be34408b3c173c5bd177938
David Bruant <bruant.d@gmail.com>
2015-05-27 18:05:45 +0200

    query layout

--- database/index.js
+++ database/index.js
@@ -3,5 +3,20 @@
 module.exports = {
     RecyclingCenters: require('./models/recycling-centers.js'),
     Sensors: require('./models/sensors.js'),
-    SensorMeasurements: require('./models/sensor-measurements.js')
+    SensorMeasurements: require('./models/sensor-measurements.js'),
+    complexQueries: {
+        currentRecyclingCenterAffluences: function(){
+            /*
+                for all recycling centers,
+                    find the sensor,
+                        for each sensor,
+                            find the most recent measurement
+                            
+                recyclingCenters
+                    .select(recyclingCenter.star(), )
+                    .from(recyclingCenter, sensor, sensorMeasurement)
+                    .toQuery();
+            */
+        }
+    }
 };

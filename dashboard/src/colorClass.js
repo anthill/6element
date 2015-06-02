ae8d45ade1443d1da4f75edd01570d6bd3160e0b
Alexandre Vallette <vallettea@gmail.com>
2015-06-02 17:21:45 +0200

    cleaned old directories

--- dashboard/src/colorClass.js
+++
@@ -1,7 +0,0 @@
-(function(global){
-    
-    global.color = d3.scale.quantize()
-        .domain([50, 600])
-        .range(d3.range(11).map(function(d) { return "q" + d + "-11"; }));
-    
-})(this);

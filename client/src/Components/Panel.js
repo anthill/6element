103ca5d5b64efc6722407c80963fb0ceaccaef7c
David Bruant <bruant.d@gmail.com>
2015-06-03 15:56:24 +0200

    recycling center details are fetched from DB and sent to client side

--- client/src/Components/Panel.js
+++ client/src/Components/Panel.js
@@ -3,57 +3,34 @@
 var React = require('react');
 
 /*
-interface Panel Props{
-    version: string,
-    selectedRecyclingCenter: {recyclingCenter_infos},
-    selectedDate: Date(),
-    dateEntriesMap: Map (date -> value),
-    predictions: {recyclingCenter_predictions},
-    onDateSelection: function -> void,
-    onVersionSelection: function -> change version and/or predict
+
+interface PanelProps{
+    recyclingCenter: RecyclingCenter
 }
-interface Panel State{
-    selectedTab: integer
+interface PanelState{
+
 }
+
 */
 
 var Panel = React.createClass({
 
     getInitialState: function(){
-        return {
-            selectedTab: 0
-        };
+        return {}
     },
 
     render: function() {
-
         var self = this;
         var props = this.props;
         var state = this.state;
 
-        // console.log('PANEL state', this.state);
-        // console.log('PANEL props', props);
-        // console.log('PANEL props.selectedRecyclingCenter', props.selectedRecyclingCenter);
-
-        // build tabs
-        /*var tabs = new Tabs({
-            tabNames: ['Predictions'],
-            selectedTab: state.selectedTab,
-            onTabChange: function(tabNumber){
-                // console.log('tabnumber', tabNumber);
-                self.setState({
-                    selectedTab: tabNumber
-                });
-            }
-        });*/
-
-        // else {
-        //     tabContent = new AnalyseContent({
-        //         recyclingCenterInfos: props.selectedRecyclingCenter
-        //     });
-        // }
-
-        return React.DOM.div({id: 'panel'}, '');
+        console.log('panel props', props);
+        
+        return React.DOM.div({id: 'panel'}, [
+            props.recyclingCenter ?
+                React.DOM.h2({}, props.recyclingCenter.name + ' ' + props.recyclingCenter.details.length) :
+                undefined
+        ]);
     }
 });
 

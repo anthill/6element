dd78618958b1468df464a54d4c17235ca2a99167
Alexandre Vallette <vallettea@gmail.com>
2015-06-04 17:05:38 +0200

    added more sensors

--- server/hardCodedSensors.js
+++ server/hardCodedSensors.js
@@ -18,6 +18,24 @@ module.exports = function(){
             // random location around Bordeaux
             lat: 44.837883, 
             lon: -0.575538
+        }),
+        database.RecyclingCenters.create({
+            name: "The North Face",
+            // random location around Bordeaux
+            lat: 44.839720, 
+            lon: -0.578130
+        }),
+        database.RecyclingCenters.create({
+            name: "Ponpon",
+            // random location around Bordeaux
+            lat: 44.845449, 
+            lon: -0.577541
+        }),
+        database.RecyclingCenters.create({
+            name: "Région Aquitaine",
+            // random location around Bordeaux
+            lat: 44.843099,
+            lon: -0.571126
         })
     ])
     .then(function(rcIds){
@@ -31,6 +49,21 @@ module.exports = function(){
                 name: "ant2",
                 installed_at: rcIds[1],
                 phone_number: "+33783720119"
+            }),
+            database.Sensors.create({
+                name: "ant3",
+                installed_at: rcIds[2],
+                phone_number: "+33783609060"
+            }),
+            database.Sensors.create({
+                name: "ant4",
+                installed_at: rcIds[3],
+                phone_number: "+33781899027"
+            }),
+            database.Sensors.create({
+                name: "ant5",
+                installed_at: rcIds[4],
+                phone_number: "+33783959384"
             })
         ])
 

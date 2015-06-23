0b19b664dbc1cd1fd864eafbc0802c3dfae1c531
Alexandre Vallette <vallettea@gmail.com>
2015-06-23 17:19:28 +0200

    livereload still not working but no error

--- app/gulpfile.js
+++ app/gulpfile.js
@@ -2,7 +2,7 @@
 
 var gulp = require('gulp');
 var server = require('gulp-express');
-// var livereload = require('gulp-livereload');
+var livereload = require('gulp-livereload');
 var browserify = require('browserify');
 var source = require("vinyl-source-stream");
 var watchify = require('watchify');
@@ -40,20 +40,20 @@ function bundleShare(b) {
   b.bundle()
    .pipe(source('browserify-bundle.js'))
    .pipe(gulp.dest('./client/'))
-   // .pipe(livereload())
    .on('error', function (err) {
       console.log(err.message);
    });
 }
 
 gulp.task('dev', ['servedev', 'watchify'], function(){
-   // livereload.listen();
-   gulp.watch("./client/src/*", ["watchify"]);
+   livereload.listen(1234);
+   gulp.watch("./client/src/*", ["watchify"], function(e){
+      livereload.changed(e.path);
+   });
 });
 
 gulp.task('prod', ['serveprod', 'watchify']);
 
 gulp.task('default', ['servedev', 'watchify'], function(){
-   // livereload.listen();
    gulp.watch("./client/src/*", ["watchify"]);
 });

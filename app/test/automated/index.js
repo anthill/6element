35fb68909cf0fd926ae95d3be71a7d3162fa95c4
David Bruant <bruant.d@gmail.com>
2015-07-16 14:57:23 +0200

    one test

---
+++ app/test/automated/index.js
@@ -0,0 +1,12 @@
+"use strict";
+
+var assert = require('chai').assert;
+
+
+describe('yo', function(){
+    
+    it('should pass easily', function(){
+        assert.strictEqual(1+1, 2);
+    });
+
+});

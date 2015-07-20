97e5eb9e20c4e491940634b623b21a72013e0b83
Alexandre Vallette <vallettea@gmail.com>
2015-07-20 22:31:29 +0200

    change db name simpler this way

--- app/database/management/connectToDB.js
+++ app/database/management/connectToDB.js
@@ -2,7 +2,7 @@
 
 var pg = require('pg');
 
-var conString = "postgres://" + process.env.POSTGRES_USER + ":" + process.env.POSTGRES_PASSWORD + "@" + process.env.DB_PORT_5432_TCP_ADDR + ":" + process.env.DB_PORT_5432_TCP_PORT + "/" + process.env.POSTGRES_USER;
+var conString = process.env.POSTGRES_CONSTRING;
 		
 module.exports = function(){
     return new Promise(function(resolve, reject){

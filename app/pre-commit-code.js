bf0b095967dc9a76965045db261e9da015867535
David Bruant <bruant.d@gmail.com>
2015-07-21 10:58:59 +0200

    eslint plumbing (rc, ignore, githook)

---
+++ app/pre-commit-code.js
@@ -0,0 +1,17 @@
+"use strict";
+
+var join = require('path').join
+var eslint = require('eslint');
+// 'git ls-files -om --exclude-standard' to list all files that were changed then filter out non-JS files
+
+process.chdir(__dirname);
+
+var exitCode = eslint.cli.execute("./");
+
+/*
+ * Wait for the stdout buffer to drain.
+ * See https://github.com/eslint/eslint/issues/317
+ */
+process.on("exit", function() {
+    process.exit(exitCode);
+});

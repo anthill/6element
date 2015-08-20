53c8a767326ea665fe712f6ed8db697e29e4fc3f
Alexandre Vallette <vallettea@gmail.com>
2015-08-20 12:02:33 +0200

    added secret key for access

--- core/server/admin.js
+++ core/server/admin.js
@@ -21,7 +21,7 @@ var zlib = require('zlib');
 
 var PORT = 4001;
 var DEBUG = process.env.NODE_ENV === "development" ? true : false;
-
+var secret = "99htdl5wBQIyVVScSEoHdqiR18eFu9EnVHAAwW6Z";
 
 var endpointConfig =
     {
@@ -105,7 +105,10 @@ app.use("/Admin", express.static(path.join(__dirname, '../clients/Admin')));
 app.use("/_common", express.static(path.join(__dirname, '../clients/_common')));
 
 app.get('/', function(req, res){
-    res.sendFile(path.join(__dirname, '../clients/Admin/index.html'));
+    if(req.query.s === secret)
+        res.sendFile(path.join(__dirname, '../clients/Admin/index.html'));
+    else
+        res.status(404).sendFile(path.join(__dirname, '../clients/Admin/403.html'));
 });
 
 
@@ -241,7 +244,9 @@ app.post('/createSensor', function(req, res){
 server.listen(PORT, function () {
     console.log('Server running on', [
         'http://localhost:',
-        PORT
+        PORT,
+        '/?s=',
+        secret
     ].join(''));
 });
 

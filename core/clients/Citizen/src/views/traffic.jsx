41a1c5ab5881c637658b761cc9983148005ca40a
Serge Bredin <serge@preuvesetconcepts.com>
2015-09-21 17:28:00 +0200

    update message

--- core/clients/Citizen/src/views/traffic.jsx
+++ core/clients/Citizen/src/views/traffic.jsx
@@ -5,7 +5,7 @@ var React = require('react');
 module.exports = React.createClass({
   render: function() {
     return (
-      <div className="traffic">Lalala</div>
+      <div className="traffic">to be continued...</div>
     );
   }
 });

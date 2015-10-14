2feb831971b98af9f5567d32aabd5bb6bfcdedd8
Alexandre Vallette <vallettea@gmail.com>
2015-10-14 14:00:24 +0200

    filters in db

--- server/database/models/places.js
+++ server/database/models/places.js
@@ -7,6 +7,10 @@ var connectToDB = require('../management/connectToDB.js');
 var places = require('../management/declarations.js').places;
 var networks = require('../management/declarations.js').networks;
 
+var jsArrayToPg = function(nodeArray) {
+    return "ARRAY['" + categories.join("','") + "']";
+}
+
 module.exports = {
 
     createByChunk: function (datas) {
@@ -40,10 +44,11 @@ module.exports = {
         return connectToDB().then(function (db) {
             
             var strDistance = "st_distance_sphere(places.geom, st_makepoint(" + coords.lon + ", " + coords.lat + ")) AS distance";
+            var filters = categories[0] === "All" ? "": " AND  places.objects ?| " + jsArrayToPg(categories);
             var query = places
                 .select(places.star(),networks.name.as('file'), networks.color.as('color'), strDistance)
                 .from(places.join(networks).on(places.network.equals(networks.id)))
-                .where("places.geom && ST_MakeEnvelope(" + bbox.minLon + ", " + bbox.minLat + ", " + bbox.maxLon + ", " + bbox.maxLat + ", 4326) AND places.objects ?| " + categories)
+                .where("places.geom && ST_MakeEnvelope(" + bbox.minLon + ", " + bbox.minLat + ", " + bbox.maxLon + ", " + bbox.maxLat + ", 4326)" + filters)
                 .order("distance")
                 .limit(100)
                 .toQuery();
@@ -66,13 +71,16 @@ module.exports = {
         return connectToDB().then(function (db) {
             
             var strDistance = "st_distance_sphere(places.geom, st_makepoint(" + coords.lon + ", " + coords.lat + ")) AS distance";
+            var filters = categories[0] === "All" ? "TRUE": "places.objects ?| " + jsArrayToPg(categories);
             var query = places
                 .select(places.star(),networks.name.as('file'), networks.color.as('color'), strDistance)
                 .from(places.join(networks).on(places.network.equals(networks.id)))
-                .where("places.objects ?| " + categories)
+                .where(filters)
                 .order("distance")
                 .limit(k)
                 .toQuery();
+
+            console.log(query)
 
             return new Promise(function (resolve, reject) {
                 db.query(query, function (err, result) {

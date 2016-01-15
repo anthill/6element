deba135e1446eb5a2752c416ff54acab9bc31551
Romain Crestey <rcrestey@gmail.com>
2016-01-15 17:38:09 +0100

    started lint cleaning, leaving the rest to serge

--- client/js/panels.js
+++ client/js/panels.js
@@ -7,16 +7,16 @@ function setActivePanel(e){
 	var panelCanvas = document.getElementById('panel-canvas-'+id);
 	var panelInfos 	= document.getElementById('panel-infos-'+id);
 	
-	panelCanvas.className = panelCanvas.className === "panel-active" ? "panel-hidden" : "panel-active";
-	panelInfos.className  = panelInfos.className  === "panel-active" ? "panel-hidden" : "panel-active";
-	e.srcElement.src = panelCanvas.className === "panel-active" ? "../img/infos.svg" : "../img/chart.svg"; 	
+	panelCanvas.className = panelCanvas.className === 'panel-active' ? 'panel-hidden' : 'panel-active';
+	panelInfos.className  = panelInfos.className  === 'panel-active' ? 'panel-hidden' : 'panel-active';
+	e.srcElement.src = panelCanvas.className === 'panel-active' ? '../img/infos.svg' : '../img/chart.svg'; 	
 }
 
 function initializeAllPanels(){
-	var list = document.getElementsByClassName("place-infos");
+	var list = document.getElementsByClassName('place-infos');
 	for(var i= 0; i<list.length; ++i){
 		var button = list[i]; 
-		button.addEventListener("click", setActivePanel);
+		button.addEventListener('click', setActivePanel);
 	}
 }
 

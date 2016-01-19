592c016c84d57bfab1def0df4ef692903f79f33c
Serge Bredin <serge@preuvesetconcepts.com>
2016-01-19 18:19:07 +0100

    fix issue with favorite on Firefox + now date on avatars

--- client/js/favorite.js
+++ client/js/favorite.js
@@ -17,10 +17,10 @@ function getCookie(sName) {
 
 
 function changeFavorite(e){
-	var button = e.srcElement.parentNode;
+	var button = e.currentTarget;
 	var isFavorite = button.class === 'place-favorite';
 	button.class = isFavorite ? 'place-no-favorite' : 'place-favorite';
-	e.srcElement.src = isFavorite ? '../img/no-favorite.svg' : '../img/favorite.svg';
+	e.currentTarget.firstChild.src = isFavorite ? '../img/no-favorite.svg' : '../img/favorite.svg';
 	var id = button.id.replace('register-','');
 	var cookie_places = getCookie('6element-places') || '';
 	if(isFavorite) 	setCookie('6element-places', cookie_places.replace(id + ';',''));// remove place in cookie

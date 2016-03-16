a60e43854cc2231ecc189d83f713ad7fff5f1a9b
Romain Crestey <rcrestey@gmail.com>
2016-03-16 16:04:41 +0100

    cleaned osmLoader

--- server/osmLoader.js
+++ server/osmLoader.js
@@ -6,16 +6,6 @@ var osm = require('query-overpass');
 var queryDic = require('./query.json');
 var converter = require('./wayToPointConverter.js');
 
-var lon = -0.570778;
-var lat = 44.8472;
-
-// var bbox = {maxLat: 44.853009071034684, minLat: 44.82963909690866, maxLon: -0.517730712890625, minLon: -0.627593994140625};
-// var bbox = {maxLat: lat + 0.5, minLat: lat - 0.5, maxLon: lon + 0.5, minLon: lon - 0.5};
-
-// console.log('bbox', bbox);
-
-
-
 module.exports = function(bbox){
 
 	var bboxCoords = [bbox.south, bbox.west, bbox.north, bbox.east].join(',');

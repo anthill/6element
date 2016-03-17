56ac53373ba2c7f9827abc9f5acbbeca6d3f05ea
Romain Crestey <rcrestey@gmail.com>
2016-03-17 10:58:08 +0100

    removed places rate property

--- server/toGeoJson.js
+++ server/toGeoJson.js
@@ -33,7 +33,6 @@ module.exports = function(results){
                         type: 'Feature',
                         properties: result,
                         geometry: { "type": "Point", "coordinates": {"lat":result["lat"], "lon": result["lon"]} },
-                        rate: 3 
                     }
                     resolve(geoJson);
                 })

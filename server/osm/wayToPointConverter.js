83a711a8d05d72bc97655ea72098f12374e1f8a2
Romain Crestey <rcrestey@gmail.com>
2016-03-24 13:54:29 +0100

    cleaned

--- server/osm/wayToPointConverter.js
+++ server/osm/wayToPointConverter.js
@@ -5,8 +5,6 @@ var centroid = require('turf-centroid');
 module.exports = function(entries){
 
 	return entries.map(function(entry){
-		entry.properties.file = 'Réseau Openstreetmap';
-		entry.properties.color = 'rgb(45, 184, 222)';
 
 		if (entry.properties.type === 'way'){
 			var point = centroid(entry);

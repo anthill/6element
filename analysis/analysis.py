0c8ab9f1ef5c5d9e2bb65e8e7a0419f973e112e0
Asphähyre <luka.boulagnon@epitech.eu>
2016-07-11 11:26:58 +0200

    Added comments

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-from matplotlib.pyplot import figure, show
+#from matplotlib.pyplot import figure, show
 import datetime
 import dateutil.parser
 import json
@@ -10,6 +10,7 @@ import sys
 import urllib
 #import pylab as plt
 
+# Help message
 if (len(sys.argv) == 1 or sys.argv[1] == "-h"):
     print "Usage: " + sys.argv[0] + " json_output [csv_output]"
     print
@@ -20,23 +21,21 @@ if (len(sys.argv) == 1 or sys.argv[1] == "-h"):
     print
     exit()
 
+# Loading configuration, needed for data_source and secret
 secret_json = os.path.dirname(os.path.abspath(__file__)) + "/../PRIVATE.json"
 with open(secret_json) as configuration_json:
     configuration = json.load(configuration_json)
 
-pp = pprint.PrettyPrinter(indent=4)
+# Getting infos of all places
 places_url = configuration["data_source"] + "/allPlacesInfos"
 places = json.loads(urllib.urlopen(places_url).read())
-allsensors = []
-base = datetime.datetime.today()
 
+# Retrieving datas of opening hours of each places
 opening_hours_url = configuration["data_source"] + "/sensor/getAll?s=" + configuration["secret"]
 opening_hours = json.loads(urllib.urlopen(opening_hours_url).read())
 
-csv_output = open("/dev/null", "w+")
-if (len(sys.argv) > 2):
-    csv_output = open(sys.argv[2], "w+")
-
+# This function returns the expected number of measures at a specific hour (UTC) from a particular date
+# It considers the jet lag from France, from summer or winter
 def nb_measures_expected(place_id, hour, month, day):
     hour += 1
     if (month > 3) or (month == 3 and day > 27):
@@ -50,6 +49,22 @@ def nb_measures_expected(place_id, hour, month, day):
             return 0
     return 0
 
+allsensors = []
+base = datetime.datetime.today()
+pp = pprint.PrettyPrinter(indent = 4)
+X = range(0, 24)
+
+# Analyzing dates up to 300 days back
+date_list = {}
+for x in range(0, 300):
+    date_list[(base - datetime.timedelta(days = x)).strftime("%Y-%m-%d")] = []
+
+# Opening the CSV file to save measures' counts to
+csv_output = open("/dev/null", "w+")
+if (len(sys.argv) > 2):
+    csv_output = open(sys.argv[2], "w+")
+
+# Printing head of the CSV
 csv_output.write("Place, Captor ID")
 max_all_days = {}
 for x in range(0, 300):
@@ -57,19 +72,17 @@ for x in range(0, 300):
     max_all_days[x] = 0
 csv_output.write("\n")
 
+# For each sensor
 for sensor in places:
-    csv_output.write(sensor["name"].encode('utf-8') + ", " + str(sensor["id"]))
     print "Processing sensor: \"" + sensor["name"].encode('utf-8') + "\"..."
+    csv_output.write(sensor["name"].encode('utf-8') + ", " + str(sensor["id"]))
+
+    # Loading all the measures and sorting them by date, to process them in order
     url = configuration["data_source"] + "/measurements/places?ids=" + str(sensor["id"]) + "&types=wifi"
     measures = json.loads(urllib.urlopen(url).read())
     measures.sort(key = lambda arr: arr["date"])
 
-    X = range(0, 24)
-
-    date_list = {}
-    for x in range(0, 300):
-        date_list[(base - datetime.timedelta(days = x)).strftime("%Y-%m-%d")] = []
-
+    # Inits
     last = dateutil.parser.parse("1970-01-01T00:00:00.000Z")
     before = last
     res = [0] * len(X)
@@ -77,17 +90,23 @@ for sensor in places:
     values = [0] * len(X)
     maximums = []
 
-    for measure in enumerate(measures):
+    for measure in measures:
         measure_date = dateutil.parser.parse(measure["date"])
         if (i > 0) and ((measure_date.day > last.day)
                 or (measure_date.month > last.month)):
+
+            # Add a curve with number of measures by hour
             #plt.plot(X, res)
+
             max_in_day = 0
             for j in range(0, len(X)):
+
+                # Retrieving the maximum of WiFi emitters in a day
                 if (values[j] > max_in_day):
                     max_in_day = values[j]
                 max_all_days[(base.date() - last.date()).days] = max_in_day
 
+                # Appending this hours' datas to the JSON
                 date_list[last.strftime("%Y-%m-%d")].append({
                     "datetime" : last.strftime("%Y-%m-%dT") + str(j) + ":00:00.000Z",
                     "expected" : nb_measures_expected(sensor["id"], j, last.month, last.day),
@@ -95,14 +114,17 @@ for sensor in places:
                     "nb_uniques" : res[j] - nb_duplicatas[j],
                     "max_measures" : values[j]})
 
+                # Reset
                 res[j] = 0
                 nb_duplicatas[j] = 0
                 values[j] = 0
 
+        # Counting the number of measures, and getting the maximum of WiFi emitters by hour
         res[measure_date.hour] += 1
         if (values[measure_date.hour] < len(measure["value"])):
             values[measure_date.hour] = len(measure["value"])
 
+        # Modifying array of maximums
         if (len(maximums) < 10) or (maximums[0]["nb"] < len(measure["value"])):
             maximums.append({
                 "nb" : len(measure["value"]),
@@ -111,22 +133,26 @@ for sensor in places:
             if (len(maximums) > 10):
                 maximums = maximums[len(maximums) - 10:]
 
+        # Checking if we've got measures from the same date twice
         if (before == measure_date):
             nb_duplicatas[measure_date.hour] += 1
         before = measure_date
         if (int(last.strftime("%Y%m%d%H")) != int(measure_date.strftime("%Y%m%d%H"))):
             last = measure_date
 
+    # Appending to JSON all data of the concerned sensor
     allsensors.append({
         "sensor" : sensor["id"],
         "name" : sensor["name"].encode("utf-8"),
         "measures" : date_list,
         "max_measures" : maximums})
 
+    # Printing maximums of WiFi emitters for these 300 days back
     for x in range(0, 300):
         csv_output.write(", " + str(max_all_days[x]))
     csv_output.write("\n");
 
+    # Preparing graphs
     #plt.title("Captor " + str(sensor["id"]) + " - " + sensor["name"])
     #plt.figure()
 
--- analysis/monitor.py
+++ analysis/monitor.py
@@ -20,10 +20,13 @@ if (len(sys.argv) < 2):
 
 place_id = sys.argv[1]
 
+# Opening configuration, needed for selecting the API's source
 secret_json = os.path.dirname(os.path.abspath(__file__)) + "/../PRIVATE.json"
 with open(secret_json) as configuration_json:
     configuration = json.load(configuration_json)
 
+# Loading measures for the selected place, and sorting them by date
+# beacause we will need to process each measure in order
 url = configuration["data_source"] + "/measurements/places?ids=" + place_id + "&types=wifi"
 measures = json.loads(urllib.urlopen(url).read())
 measures.sort(key = lambda arr: arr["date"])
@@ -33,10 +36,17 @@ res = [0] * len(X)
 last = datetime.datetime.fromtimestamp(0)
 before = last
 
+# Processing for each measure
 for measure in enumerate(measures):
     measures_date = dateutil.parser.parse(measure["date"])
+
+    # If the date has changed, we have to process the last day's data
     if (measure_date.day > last.day) or (measure_date.month > last.month):
+
+        # Adding plot to the chart
         plt.plot(X, res)
+        
+        # Display contradictions in measures counts, by hour
         for index, nb_in_an_hour in enumerate(res):
             date_log = last.strftime("%Y-%m-%d") + ", at " + str(index) + " o'clock, with " + str(nb_in_an_hour) + " measures"
             if ((nb_in_an_hour < 12) and (nb_in_an_hour > 0)):
@@ -45,6 +55,7 @@ for measure in enumerate(measures):
                 print "Too many measures on day " + date_log
             res[index] = 0
 
+    # Counting the measure on the current day, in the concerned hour
     res[measure_date.hour] += 1
 
     if (int(last.strftime("%Y%m%d%H")) != int(measure_date.strftime("%Y%m%d%H"))):
